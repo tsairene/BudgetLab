3fb503db1e6bff89a57cff988abbc8c3de9a77c7
ReneTsai <rene.tsai@linecorp.com>
2018-08-28 17:16:57 +0800

    add budget_return_with_decimal_in_2_month() test

--- src/BudgetServiceTest.java
+++ src/BudgetServiceTest.java
@@ -32,6 +32,14 @@ public class BudgetServiceTest {
     }
 
     @Test
+    public void budget_return_with_decimal_in_2_month() {
+        BudgetService budgetService = new BudgetService();
+        LocalDate startDate = LocalDate.of(2018, 3, 1);
+        LocalDate endDate = LocalDate.of(2018, 4, 1);
+        assertEquals(1002.67, budgetService.queryBudget(startDate, endDate), 0.001);
+    }
+
+    @Test
     public void get_budget_by_date() {
         final BudgetService budgetService = new BudgetService();
         final LocalDate testDate = LocalDate.of(2018, 8, 1);
