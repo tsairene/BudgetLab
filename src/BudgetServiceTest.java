bf7017ec2d2031a90ecfd348b91c8d523dff4f76
ReneTsai <rene.tsai@linecorp.com>
2018-08-28 18:30:27 +0800

    refactor BudgetServiceTest and extract budgetShouldBe() method

--- src/BudgetServiceTest.java
+++ src/BudgetServiceTest.java
@@ -5,58 +5,56 @@ import java.time.LocalDate;
 import static org.junit.Assert.*;
 
 public class BudgetServiceTest {
+    BudgetService budgetService = new BudgetService();
 
     @Test
-    public void same_date() {
-        BudgetService budgetService = new BudgetService();
+    public void budget_in_same_date() {
         LocalDate startDate = LocalDate.of(2018, 8, 1);
-        assertEquals(19.03, budgetService.queryBudget(startDate, startDate), 0.001);
+        budgetShouldBe(19.03f, budgetService.queryBudget(startDate, startDate));
     }
 
     @Test
-    public void same_month() {
-        BudgetService budgetService = new BudgetService();
+    public void budget_in_same_month() {
         LocalDate startDate = LocalDate.of(2018, 8, 2);
         LocalDate endDate = LocalDate.of(2018, 8, 3);
-        assertEquals(38.06, budgetService.queryBudget(startDate, endDate), 0.001);
+        budgetShouldBe(38.06f, budgetService.queryBudget(startDate, endDate));
     }
 
     @Test
     public void budget_return_with_decimal_in_same_month() {
-        BudgetService budgetService = new BudgetService();
         LocalDate startDate = LocalDate.of(2018, 3, 1);
         LocalDate endDate = LocalDate.of(2018, 3, 1);
-        assertEquals(31.61, budgetService.queryBudget(startDate, endDate), 0.001);
+        budgetShouldBe(31.61f, budgetService.queryBudget(startDate, endDate));
     }
 
     @Test
     public void budget_return_with_decimal_in_2_month() {
-        BudgetService budgetService = new BudgetService();
         LocalDate startDate = LocalDate.of(2018, 3, 1);
         LocalDate endDate = LocalDate.of(2018, 4, 1);
-        assertEquals(1002.67, budgetService.queryBudget(startDate, endDate), 0.001);
+        budgetShouldBe(1002.67f, budgetService.queryBudget(startDate, endDate));
     }
 
     @Test
     public void budget_return_with_decimal_in_2_year() {
-        BudgetService budgetService = new BudgetService();
         LocalDate startDate = LocalDate.of(2018, 3, 1);
         LocalDate endDate = LocalDate.of(2019, 2, 28);
-        assertEquals(8183, budgetService.queryBudget(startDate, endDate), 0.001);
+        budgetShouldBe(8183, budgetService.queryBudget(startDate, endDate));
     }
 
     @Test
     public void budget_return_with_decimal_in_2_year2() {
-        BudgetService budgetService = new BudgetService();
         LocalDate startDate = LocalDate.of(2018, 12, 31);
         LocalDate endDate = LocalDate.of(2019, 2, 28);
-        assertEquals(1156, budgetService.queryBudget(startDate, endDate), 0.001);
+        budgetShouldBe(1156, budgetService.queryBudget(startDate, endDate));
     }
 
     @Test
     public void get_budget_by_date() {
-        final BudgetService budgetService = new BudgetService();
         final LocalDate testDate = LocalDate.of(2018, 8, 1);
-        assertEquals(590, budgetService.getMonthBudgetOfDate(testDate), 0.001);
+        budgetShouldBe(590, budgetService.getMonthBudgetOfDate(testDate));
+    }
+
+    private void budgetShouldBe(float expected, float actual) {
+        assertEquals(expected, actual, 0.001);
     }
 }
